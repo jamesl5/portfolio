757fd9e6471e915f37d230429b2e2fa1c2a5500d
jamesl5@uw.edu <jamesl5@uw.edu>
2015-11-23 23:18:33 +0900

    comments and styling

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,8 +1,5 @@
 // Create app
 angular.module('myApp',['ui.router', 'firebase'])
-//angular.module('myApp.firebase', ['firebase'])
-//angular.module('myApp.router', ['ui.router'])
-// Configure app
 // Config route provider
 .config(function($stateProvider) {
     $stateProvider
@@ -11,7 +8,6 @@ angular.module('myApp',['ui.router', 'firebase'])
             templateUrl: 'templates/landing.html',
             controller: 'HomeController',
         })
-        // Configure states for "content" and "about"
         .state('content', {
             url:'/content',
             templateUrl: 'templates/content.html',
@@ -25,20 +21,20 @@ angular.module('myApp',['ui.router', 'firebase'])
     })
 
 
-// Landing page controller: define $scope.number as a number
+// Homepage controller
 .controller('HomeController', function($scope){
 	$scope.title = "James Lee's Portfolio"
 	$scope.introduction = "This is a portfolio website for INFO343. The portfolio contains challenges that are completed during Fall 2015. Please email me if you have any questions."
 })
 
-// About page controller: define $scope.about as a string
+// About page controller
 .controller('ProfileController', function($scope){
 	$scope.DoB = "Jul 5, 1990"
 	$scope.Name = "James Lee"
 	$scope.Email = "lee.james1990@gmail.com"
 })
 
-// Content controller: define $scope.url as an image
+// Content page controller
 .controller('PortfolioController', function($scope, $firebaseArray){
 	var ref = new Firebase("https://personalportfolio.firebaseio.com/");
 	var contentsRef = ref.child("contents");
